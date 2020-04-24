6ba1c8bd48e6abbbf56789abba359d6cda71859f
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-24 11:16:37 -0400

    Add users-model

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get("/", (req, res) => {
+    res.json({ api: "Up" });
+})
+
 module.exports = server;
---
+++ auth/users-model.js
@@ -0,0 +1,20 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+    add,
+    findBy,
+    findById
+};
+
+async function add(user) {
+    const [id] = await db("users").insert(user, "id");
+    return findById(id);
+}
+
+function findBy(filter) {
+    return db("users").where(filter);
+}
+
+function findById(id) {
+    return db("users").where("id", id).first();
+}
