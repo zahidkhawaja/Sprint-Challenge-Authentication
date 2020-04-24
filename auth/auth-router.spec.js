af3d72073f9c7932731a723d37ecceda5689fe9a
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-24 13:18:43 -0400

    Remove superfluous comments

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -3,32 +3,28 @@ const server = require("../api/server");
 const db = require("../database/dbConfig");
 
 describe("auth test suite", () => {
+    // Clear the users table before testing
     beforeAll(async () => {
         await db("users").truncate();
     });
 
     it("Register should return a 201 ok if username is available", () => {
-        // We cleared the users table first to start with a clean slate
         return request(server).post("/api/auth/register").send({ "username": "whew", "password": "meh" })
-        // First registration, should be ok
         .then(res => expect(res.status).toBe(201));
     })
 
     it("Register should return a 500 error if username is taken", () => {
         return request(server).post("/api/auth/register").send({ "username": "whew", "password": "meh" })
-        // Trying to register again with the same username, should fail
         .then(res => expect(res.status).toBe(500));
     })
 
     it("Login should return a 200 ok if password is correct", () => {
         return request(server).post("/api/auth/login").send({ "username": "whew", "password": "meh" })
-        // Trying to login with the correct password 'meh', should be ok
         .then(res => expect(res.status).toBe(200));
     })
 
     it("Login should return a 401 error if password is incorrect", () => {
         return request(server).post("/api/auth/login").send({ "username": "whew", "password": "mah" })
-        // Trying to login with the wrong password 'mah', should fail
         .then(res => expect(res.status).toBe(401));
     })
 });
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,20 +2,16 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
-  // Use a real secret in production
   const secret = process.env.JWT_SECRET || "justtestinglocally";
 
-  // If token exists, verify it
+  // Verify token if it exists
   token ? jwt.verify(token, secret, (error, decodedToken) => {
-    // Auth failure
     if(error) {
       res.status(401).json({ message: "Cannot authenticate" });
     } else {
-      // Auth successful, allow access
       req.decodedToken = decodedToken;
       next();
     }
   })
-  // If no token exists
   : res.status(400).json({ message: "You must be authenticated" });
 };
